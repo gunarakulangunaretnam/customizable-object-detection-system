2d75102997048c4b17a9dec9c6ad2c50db6332f6
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-08 14:51:32 +0530

    Add download, extract & delete functions to model downloader

--- download-models.py
+++ download-models.py
@@ -1,5 +1,7 @@
 import os
+import time
 import shutil
+import tarfile
 import argparse
 import requests
 from tqdm import tqdm
@@ -7,22 +9,6 @@ from tqdm import tqdm
 ap = argparse.ArgumentParser()
 ap.add_argument("-u", "--url", required=True, help="URL of the file")
 args = vars(ap.parse_args())
-
-
-def create_directory(target_url):
-    folder_name = target_url.split("/")
-    folder_name = folder_name[-1].split(".")[0]
-
-    dirpath = os.path.join('data/models', folder_name)
-
-    if os.path.exists(dirpath) and os.path.isdir(dirpath):
-        shutil.rmtree(dirpath)
-
-    os.mkdir(f'data/models/{folder_name}')
-
-
-create_directory(args["url"])
-
 
 def download(url: str, fname: str):
     resp = requests.get(url, stream=True)
@@ -38,5 +24,20 @@ def download(url: str, fname: str):
             size = file.write(data)
             bar.update(size)
 
-            
-#download("http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz", "model12")
+def create_directory(target_url):
+
+    file_name = target_url.split("/")[-1]
+    directory_structure_path = f'data/models/{file_name}'
+    download(target_url, directory_structure_path)
+
+    file = tarfile.open(directory_structure_path)
+
+    # extracting file
+    file.extractall(f'data/models/')
+    file.close()
+
+    if os.path.exists(directory_structure_path):
+        os.remove(directory_structure_path)  # Delete config folder
+
+
+create_directory(args["url"])
