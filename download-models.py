a4a23b1a9e36e50e244ea98f5cc5c7bb20623f70
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-08 12:29:10 +0530

    Create initial files and directories

---
+++ download-models.py
@@ -0,0 +1,18 @@
+from tqdm import tqdm
+import requests
+
+chunk_size = 1024
+
+url = "http://www.nervenet.org/pdf/python3handson.pdf"
+
+r = requests.get(url, stream = True)
+
+total_size = int(r.headers['content-length'])
+filename = url.split('/')[-1]
+
+with open(filename, 'wb') as f:
+    for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'KB'):
+        f.write(data)
+
+
+print("Download complete!")
