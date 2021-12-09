22f43e9e2b6a4508735db82b824cbd3a5d226d52
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-10 00:58:50 +0530

    Cleanup textbox after downloading

--- project-files/object-detection-alerting-system/bin/Debug/download-models.bat
+++ project-files/object-detection-alerting-system/bin/Debug/download-models.bat
@@ -1,1 +1,1 @@
-python download-models.py -u http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d2_coco17_tpu-32.tar.gz
+python download-models.py -u http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v1_fpn_640x640_coco17_tpu-8.tar.gz
