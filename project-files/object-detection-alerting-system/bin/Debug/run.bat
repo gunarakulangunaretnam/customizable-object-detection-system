34d6b326df6dbb83ecd8102bffd862c76f25d8c7
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-31 13:02:05 +0530

    Update demo iamge

--- project-files/object-detection-alerting-system/bin/Debug/run.bat
+++ project-files/object-detection-alerting-system/bin/Debug/run.bat
@@ -1,1 +1,1 @@
-python run.py -m ssd_mobilenet_v2_320x320_coco17_tpu-8 -l elephant -a [TRUE] -t 60 -s "[WEBCAM]|2"
+python run.py -m ssd_mobilenet_v2_320x320_coco17_tpu-8 -l person -a [TRUE] -t 50 -s "[WEBCAM]|0"
